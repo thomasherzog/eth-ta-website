bd23c13101536999af6f11a559d2cab94febf19a
Thomas Herzog <batthomas@gmx.ch>
2026-02-15 19:02:55 +0100

    Correct base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://thomasherzog.github.io',
+  base: '/eth-ta-website',
   vite: {
     plugins: [tailwindcss()]
   }
